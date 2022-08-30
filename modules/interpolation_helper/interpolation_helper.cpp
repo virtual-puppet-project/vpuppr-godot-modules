8c8aff0b8d79e33168ecc45b765381c4183781db
Tim Yuen <timothy.yuen22@gmail.com>
2022-08-30 18:06:04 -0400

    expose damping

--- modules/interpolation_helper/interpolation_helper.cpp
+++ modules/interpolation_helper/interpolation_helper.cpp
@@ -1,84 +1,92 @@
 #include "interpolation_helper.h"
 
-void InterpolationHelper::set_both_rates(float p_new_value) {
-    if (should_interpolate) {
-        interpolation_rate = p_new_value;
-    }
-    last_interpolation_rate = p_new_value;
+void InterpolationHelper::set_both_rates(const float p_new_value) {
+	if (should_interpolate) {
+		interpolation_rate = p_new_value;
+	}
+	last_interpolation_rate = p_new_value;
 }
 
-void InterpolationHelper::global_rate_changed(float p_new_value) {
-    if (!should_interpolate) {
-        interpolation_rate = p_new_value;
-    }
+void InterpolationHelper::global_rate_changed(const float p_new_value) {
+	if (!should_interpolate) {
+		interpolation_rate = p_new_value;
+	}
 }
 
-void InterpolationHelper::maybe_reset_rate(float p_global_rate) {
-    if (should_interpolate) {
-        interpolation_rate = last_interpolation_rate;
-    } else {
-        last_interpolation_rate = interpolation_rate;
-        interpolation_rate = p_global_rate;
-    }
+void InterpolationHelper::maybe_reset_rate(const float p_global_rate) {
+	if (should_interpolate) {
+		interpolation_rate = last_interpolation_rate;
+	} else {
+		last_interpolation_rate = interpolation_rate;
+		interpolation_rate = p_global_rate;
+	}
 }
 
 Variant InterpolationHelper::interpolate(const float p_rate) {
-    last_value = interpolate_no_update(p_rate);
+	last_value = interpolate_no_update(p_rate);
 
-    return last_value;
+	return last_value;
 }
 
 Variant InterpolationHelper::interpolate_no_update(const float p_rate) {
-    switch (target_value.get_type()) {
-        case Variant::REAL:
-            return Variant(lerp(float(last_value), float(target_value) * damping, p_rate));
-        case Variant::VECTOR2:
-            return Variant(lerp(Vector2(last_value), Vector2(target_value) * damping, p_rate));
-        case Variant::VECTOR3:
-            return Variant(lerp(Vector3(last_value), Vector3(target_value) * damping, p_rate));
-        default:
-            return Variant();
-    }
+	switch (target_value.get_type()) {
+		case Variant::REAL:
+			return Variant(lerp(float(last_value), float(target_value) * damping, p_rate));
+		case Variant::VECTOR2:
+			return Variant(lerp(Vector2(last_value), Vector2(target_value) * damping, p_rate));
+		case Variant::VECTOR3:
+			return Variant(lerp(Vector3(last_value), Vector3(target_value) * damping, p_rate));
+		default:
+			return Variant();
+	}
 }
 
-void InterpolationHelper::set_should_interpolate(bool p_should_interpolate) {
-    should_interpolate = p_should_interpolate;
+void InterpolationHelper::set_should_interpolate(const bool p_should_interpolate) {
+	should_interpolate = p_should_interpolate;
 }
 
 bool InterpolationHelper::get_should_interpolate() const {
-    return should_interpolate;
+	return should_interpolate;
 }
 
 void InterpolationHelper::set_interpolation_rate(const float p_interpolation_rate) {
-    interpolation_rate = p_interpolation_rate;
+	interpolation_rate = p_interpolation_rate;
 }
 
 float InterpolationHelper::get_interpolation_rate() const {
-    return interpolation_rate;
+	return interpolation_rate;
 }
 
 void InterpolationHelper::set_last_interpolation_rate(const float p_last_interpolation_rate) {
-    last_interpolation_rate = p_last_interpolation_rate;
+	last_interpolation_rate = p_last_interpolation_rate;
 }
 
 float InterpolationHelper::get_last_interpolation_rate() const {
-    return last_interpolation_rate;
+	return last_interpolation_rate;
+}
+
+void InterpolationHelper::set_damping(const float p_damping) {
+	damping = p_damping;
+}
+
+float InterpolationHelper::get_damping() const {
+	return damping;
 }
 
 void InterpolationHelper::set_last_value(const Variant &p_last_value) {
-    last_value = p_last_value;
+	last_value = p_last_value;
 }
 
 Variant InterpolationHelper::get_last_value() const {
-    return last_value;
+	return last_value;
 }
 
 void InterpolationHelper::set_target_value(const Variant &p_target_value) {
-    target_value = p_target_value;
+	target_value = p_target_value;
 }
 
 Variant InterpolationHelper::get_target_value() const {
-    return target_value;
+	return target_value;
 }
 
 InterpolationHelper::InterpolationHelper() {}
@@ -86,32 +94,36 @@ InterpolationHelper::InterpolationHelper() {}
 InterpolationHelper::~InterpolationHelper() {}
 
 void InterpolationHelper::_bind_methods() {
-    ClassDB::bind_method(D_METHOD("set_both_rates", "new_rate"), &InterpolationHelper::set_both_rates);
-    ClassDB::bind_method(D_METHOD("global_rate_changed", "new_rate"), &InterpolationHelper::global_rate_changed);
-    ClassDB::bind_method(D_METHOD("maybe_reset_rate", "global_rate"), &InterpolationHelper::maybe_reset_rate);
-    ClassDB::bind_method(D_METHOD("interpolate", "rate"), &InterpolationHelper::interpolate);
-    ClassDB::bind_method(D_METHOD("interpolate_no_update", "rate"), &InterpolationHelper::interpolate_no_update);
+	ClassDB::bind_method(D_METHOD("set_both_rates", "new_rate"), &InterpolationHelper::set_both_rates);
+	ClassDB::bind_method(D_METHOD("global_rate_changed", "new_rate"), &InterpolationHelper::global_rate_changed);
+	ClassDB::bind_method(D_METHOD("maybe_reset_rate", "global_rate"), &InterpolationHelper::maybe_reset_rate);
+	ClassDB::bind_method(D_METHOD("interpolate", "rate"), &InterpolationHelper::interpolate);
+	ClassDB::bind_method(D_METHOD("interpolate_no_update", "rate"), &InterpolationHelper::interpolate_no_update);
 
-    ClassDB::bind_method(D_METHOD("set_should_interpolate", "should_interpolate"), &InterpolationHelper::set_should_interpolate);
-    ClassDB::bind_method(D_METHOD("get_should_interpolate"), &InterpolationHelper::get_should_interpolate);
+	ClassDB::bind_method(D_METHOD("set_should_interpolate", "should_interpolate"), &InterpolationHelper::set_should_interpolate);
+	ClassDB::bind_method(D_METHOD("get_should_interpolate"), &InterpolationHelper::get_should_interpolate);
 
-    ClassDB::bind_method(D_METHOD("set_interpolation_rate", "interpolation_rate"), &InterpolationHelper::set_interpolation_rate);
-    ClassDB::bind_method(D_METHOD("get_interpolation_rate"), &InterpolationHelper::get_interpolation_rate);
+	ClassDB::bind_method(D_METHOD("set_interpolation_rate", "interpolation_rate"), &InterpolationHelper::set_interpolation_rate);
+	ClassDB::bind_method(D_METHOD("get_interpolation_rate"), &InterpolationHelper::get_interpolation_rate);
 
-    ClassDB::bind_method(D_METHOD("set_last_interpolation_rate", "last_interpolation_rate"), &InterpolationHelper::set_last_interpolation_rate);
-    ClassDB::bind_method(D_METHOD("get_last_interpolation_rate"), &InterpolationHelper::get_last_interpolation_rate);
+	ClassDB::bind_method(D_METHOD("set_last_interpolation_rate", "last_interpolation_rate"), &InterpolationHelper::set_last_interpolation_rate);
+	ClassDB::bind_method(D_METHOD("get_last_interpolation_rate"), &InterpolationHelper::get_last_interpolation_rate);
 
-    ClassDB::bind_method(D_METHOD("set_last_value", "last_value"), &InterpolationHelper::set_last_value);
-    ClassDB::bind_method(D_METHOD("get_last_value"), &InterpolationHelper::get_last_value);
+	ClassDB::bind_method(D_METHOD("set_damping", "damping"), &InterpolationHelper::set_damping);
+	ClassDB::bind_method(D_METHOD("get_damping"), &InterpolationHelper::get_damping);
 
-    ClassDB::bind_method(D_METHOD("set_target_value", "target_value"), &InterpolationHelper::set_target_value);
-    ClassDB::bind_method(D_METHOD("get_target_value"), &InterpolationHelper::get_target_value);
+	ClassDB::bind_method(D_METHOD("set_last_value", "last_value"), &InterpolationHelper::set_last_value);
+	ClassDB::bind_method(D_METHOD("get_last_value"), &InterpolationHelper::get_last_value);
 
-    ADD_PROPERTY(PropertyInfo(Variant::BOOL, "should_interpolate"), "set_should_interpolate", "get_should_interpolate");
+	ClassDB::bind_method(D_METHOD("set_target_value", "target_value"), &InterpolationHelper::set_target_value);
+	ClassDB::bind_method(D_METHOD("get_target_value"), &InterpolationHelper::get_target_value);
 
-    ADD_PROPERTY(PropertyInfo(Variant::REAL, "interpolation_rate"), "set_interpolation_rate", "get_interpolation_rate");
-    ADD_PROPERTY(PropertyInfo(Variant::REAL, "last_interpolation_rate"), "set_last_interpolation_rate", "get_last_interpolation_rate");
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "should_interpolate"), "set_should_interpolate", "get_should_interpolate");
 
-    ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "last_value"), "set_last_value", "get_last_value");
-    ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "target_value"), "set_target_value", "get_target_value");
+	ADD_PROPERTY(PropertyInfo(Variant::REAL, "interpolation_rate"), "set_interpolation_rate", "get_interpolation_rate");
+	ADD_PROPERTY(PropertyInfo(Variant::REAL, "last_interpolation_rate"), "set_last_interpolation_rate", "get_last_interpolation_rate");
+	ADD_PROPERTY(PropertyInfo(Variant::REAL, "damping"), "set_damping", "get_damping");
+
+	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "last_value"), "set_last_value", "get_last_value");
+	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "target_value"), "set_target_value", "get_target_value");
 }
--- modules/interpolation_helper/interpolation_helper.h
+++ modules/interpolation_helper/interpolation_helper.h
@@ -4,7 +4,7 @@
 #include "core/reference.h"
 
 class InterpolationHelper : public Reference {
-    GDCLASS(InterpolationHelper, Reference);
+	GDCLASS(InterpolationHelper, Reference);
 
 	bool should_interpolate = false;
 	float interpolation_rate = 0.0;
@@ -16,18 +16,18 @@ class InterpolationHelper : public Reference {
 	Variant target_value;
 
 private:
-    _FORCE_INLINE_ float lerp(float p_from, float p_to, float p_weight) {
-        return p_from + (p_to - p_from) * p_weight;
-    }
-    _FORCE_INLINE_ Vector2 lerp(Vector2 p_from, Vector2 p_to, float p_weight) {
-        return p_from + (p_to - p_from) * p_weight;
-    }
-    _FORCE_INLINE_ Vector3 lerp(Vector3 p_from, Vector3 p_to, float p_weight) {
-        return p_from + (p_to - p_from) * p_weight;
-    }
+	_FORCE_INLINE_ float lerp(float p_from, float p_to, float p_weight) {
+		return p_from + (p_to - p_from) * p_weight;
+	}
+	_FORCE_INLINE_ Vector2 lerp(Vector2 p_from, Vector2 p_to, float p_weight) {
+		return p_from + (p_to - p_from) * p_weight;
+	}
+	_FORCE_INLINE_ Vector3 lerp(Vector3 p_from, Vector3 p_to, float p_weight) {
+		return p_from + (p_to - p_from) * p_weight;
+	}
 
 protected:
-    static void _bind_methods();
+	static void _bind_methods();
 
 public:
 	void set_both_rates(const float p_new_value);
@@ -37,23 +37,26 @@ public:
 	Variant interpolate(const float p_rate);
 	Variant interpolate_no_update(const float p_rate);
 
-    void set_should_interpolate(const bool p_should_interpolate);
-    bool get_should_interpolate() const;
+	void set_should_interpolate(const bool p_should_interpolate);
+	bool get_should_interpolate() const;
 
-    void set_interpolation_rate(const float p_interpolation_rate);
-    float get_interpolation_rate() const;
+	void set_interpolation_rate(const float p_interpolation_rate);
+	float get_interpolation_rate() const;
 
-    void set_last_interpolation_rate(const float p_last_interpolation_rate);
-    float get_last_interpolation_rate() const;
+	void set_last_interpolation_rate(const float p_last_interpolation_rate);
+	float get_last_interpolation_rate() const;
 
-    void set_last_value(const Variant &p_last_value);
-    Variant get_last_value() const;
+	void set_damping(const float p_damping);
+	float get_damping() const;
 
-    void set_target_value(const Variant &p_target_value);
-    Variant get_target_value() const;
+	void set_last_value(const Variant &p_last_value);
+	Variant get_last_value() const;
 
-    InterpolationHelper();
-    ~InterpolationHelper();
+	void set_target_value(const Variant &p_target_value);
+	Variant get_target_value() const;
+
+	InterpolationHelper();
+	~InterpolationHelper();
 };
 
 #endif // INTERPOLATION_HELPER_H
